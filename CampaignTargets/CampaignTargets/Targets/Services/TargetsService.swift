16bbacd016a78c732aa10fa0015234ebbb4c2484
Florin Velesca <velesca.florin97@gmail.com>
2024-02-17 02:54:40 +0200

    Use github data to make a real get request add json for Campaigns and Channels

--- CampaignTargets/CampaignTargets/Targets/Services/TargetsService.swift
+++ CampaignTargets/CampaignTargets/Targets/Services/TargetsService.swift
@@ -7,31 +7,29 @@
 
 import Foundation
 
+enum NetworkError: Error {
+    case urlError
+    case serverError
+}
+
 protocol TargetServiceProtocol {
     func fetchTargets() async throws -> [Target]
 }
 
 class TargetsService: TargetServiceProtocol {
     func fetchTargets() async throws -> [Target] {
-//        try await Task.sleep(nanoseconds: 1_000_000_000)
-        return mockTargets
+        guard let url = URL(string: "https://raw.githubusercontent.com/Florin97/Targets/main/CampaignTargets/Targets.json") else {
+            throw NetworkError.urlError
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NetworkError.serverError
+        }
+        
+        let targets = try JSONDecoder().decode([Target].self, from: data)
+        return targets
     }
 }
 
-let mockTargets = [
-    Target(id: "1", name: "Location"),
-    Target(id: "2", name: "Age"),
-    Target(id: "3", name: "Sex"),
-    Target(id: "4", name: "Spoken Languages"),
-    Target(id: "5", name: "Events"),
-    Target(id: "6", name: "Company"),
-    Target(id: "7", name: "Industry"),
-    Target(id: "8", name: "Expertise"),
-    Target(id: "9", name: "Education"),
-    Target(id: "10", name: "Technology"),
-    Target(id: "11", name: "Interests"),
-    Target(id: "12", name: "Device"),
-    Target(id: "13", name: "Keywords"),
-    Target(id: "14", name: "Similar Products/Services"),
-    Target(id: "15", name: "Hyperlinks")
-]
